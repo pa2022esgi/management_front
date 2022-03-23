7c0e2893ce7f63013adce4a86c8d67555ea46e79
Pierre <bonnamy.77@gmail.com>
2022-03-23 23:04:42 +0100

    login using scene builder

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,52 +1,23 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage login) throws Exception{
 
-        //test
-        Label emailLabel = new Label("Email");
-        TextField emailInput = new TextField();
-        emailInput.setPrefWidth(300);
+        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
 
-        Label pwdLabel = new Label("Password");
-        TextField pwdInput = new TextField();
-        pwdInput.setPrefWidth(300);
-
-        Button connect = new Button("Login");
-        connect.setPrefWidth(300);
-
-        GridPane gp = new GridPane();
-        gp.add(emailLabel, 0, 1);
-        gp.add(emailInput, 1, 1);
-        gp.add(pwdLabel, 0, 2);
-        gp.add(pwdInput, 1, 2);
-        gp.add(connect, 0,3,2,1);
-
-        gp.setPadding(new Insets(20));
-        gp.setHgap(25);
-        gp.setVgap(25);
-        gp.setAlignment(Pos.CENTER);
-
-        HBox hBox = new HBox(gp);
-        hBox.setAlignment(Pos.CENTER);
-
-
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(hBox, 500, 500));
-        primaryStage.show();
+        login.setTitle("Connexion");
+        login.setScene(new Scene(root, 500, 500));
+        login.setMinWidth(500);
+        login.setMinHeight(500);
+        login.show();
     }
 
 
