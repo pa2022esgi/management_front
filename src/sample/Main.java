7c850b088b3ee4d4b6941049adb1ccd426d7acb2
Pierre <bonnamy.77@gmail.com>
2022-03-23 23:25:40 +0100

    register

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage login) throws Exception{
 
-        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("register.fxml"));
 
         login.setTitle("Connexion");
         login.setScene(new Scene(root, 500, 500));
