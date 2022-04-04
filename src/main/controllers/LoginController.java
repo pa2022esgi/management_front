941c92a5b2e81ca7a2574b34b67b162c809fe427
Pierre <bonnamy.77@gmail.com>
2022-04-04 23:11:50 +0200

    using simple okhttp get request

--- src/main/controllers/LoginController.java
+++ src/main/controllers/LoginController.java
@@ -3,6 +3,9 @@ package main.controllers;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 import java.io.IOException;
 
@@ -11,7 +14,17 @@ public class LoginController {
     @FXML private Button btn_register;
 
     public void login () {
+        OkHttpClient client = new OkHttpClient();
 
+        Request request = new Request.Builder()
+            .url("http://127.0.0.1:8000/api/test")
+            .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            System.out.println(response.body().string());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void register () throws IOException {
