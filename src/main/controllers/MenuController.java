18d1ef74e12695b94994a6514cc27252c0e582c9
Pierre <bonnamy.77@gmail.com>
2022-04-10 16:28:57 +0200

    fixing style & controller

--- src/main/controllers/MenuController.java
+++ src/main/controllers/MenuController.java
@@ -1,4 +1,22 @@
 package main.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class MenuController {
+    @FXML private Button btn_boards;
+    @FXML private Button btn_new;
+    @FXML private Button btn_logout;
+
+    public void logout() {
+
+    }
+
+    public void boards_list() {
+
+    }
+
+    public void new_board() {
+
+    }
 }
